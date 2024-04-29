b9ed0fb6689a93234924c8b25ec17bb89ca01892
jay-whitwell <jay.whitwell@digital.justice.gov.uk>
2024-04-29 21:53:25 +0100

    remove files

--- vpc/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "igw" {
-  description = "The ID of the Internet Gateway"
-  value       = aws_internet_gateway.igw
-}
-
-output "nic_id" {
-  description = "The ID of the NIC for the EC2 instance"
-  value       = aws_network_interface.web-server-nic.id
-}
