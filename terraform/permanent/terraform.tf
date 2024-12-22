45c4b142567a1991f8997cfad32ab80a2f935905
Jay Whitwell <72501756+jay-whitwell@users.noreply.github.com>
2024-12-22 12:04:42 +0000

    reformat repo (#10)
    * reformat repo
    
    * show directory
    
    * filter more specifically
    
    * update logic
    
    * remove fi
    
    * update logic
    
    * ensure tf runs when called
    
    * update workdir
    
    * update paths
    
    * add checkout
    
    * change dir maybe?
    
    * show workspace
    
    * show dirs and workdirs
    
    * typo
    
    * list
    
    * remove on main
    
    * add gitkeep
    
    * change tfstate
    
    * update tf
    
    * to plan
    
    * update version
    
    * pin version
    
    * pin bump
    
    * update workflows

---
+++ terraform/permanent/terraform.tf
@@ -0,0 +1,19 @@
+terraform {
+
+  backend "s3" {
+    bucket  = "jay-tfstate"
+    key     = "permanent/terraform.tfstate"
+    region  = "eu-west-2"
+    encrypt = true
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = "1.10.3"
+}
+
